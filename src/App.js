6f38f8b351a4860a5fb67583e1d26fb46c50cf40
ritikchhimwal <ritikchhimwal822@gmail.com>
2024-03-02 20:11:19 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+    const [input,setInput] = useState('');
+
+     const handleInput = (val) => {
+        if (val === 'C'){
+            setInput('');
+        }
+        else if(val === 'X'){
+            setInput(input.slice(0,-1));
+        }
+        else if(val === '='){
+            try{
+            setInput(eval(input).toString());
+            }catch(error){
+                setInput('Invailid Argument')
+            }
+        }
+        else{
+            setInput((preValue) => preValue + val);
+        }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <div className="calc">
+            <h1 id="input">{input}</h1>
+            <div>
+                <button onClick={() =>
+                    {handleInput('C')}}>C</button>
+                <button onClick={() =>
+                    {handleInput('X')}}>X</button>
+                <button onClick={() =>
+                    {handleInput('%')}}>%</button>
+                <button onClick={() =>
+                    {handleInput('/')}}>/</button>
+            </div>
+            <div>
+                <button onClick={() =>
+                    {handleInput('7')}}>7</button>
+                <button onClick={() =>
+                    {handleInput('8')}}>8</button>
+                <button onClick={() =>
+                    {handleInput('9')}}>9</button>
+                <button onClick={() =>
+                    {handleInput('*')}}>*</button>
+            </div>
+            <div>
+                <button onClick={() =>
+                    {handleInput('4')}}>4</button>
+                <button onClick={() =>
+                    {handleInput('5')}}>5</button>
+                <button onClick={() =>
+                    {handleInput('6')}}>6</button>
+                <button onClick={() =>
+                    {handleInput('-')}}>-</button>
+            </div>
+            <div>
+                <button onClick={() =>
+                    {handleInput('1')}}>1</button>
+                <button onClick={() =>
+                    {handleInput('2')}}>2</button>
+                <button onClick={() =>
+                    {handleInput('3')}}>3</button>
+                <button onClick={() =>
+                    {handleInput('+')}}>+</button>
+            </div>
+            <div>
+                <button onClick={() =>
+                    {handleInput('8')}}>0</button>
+                <button onClick={() =>
+                    {handleInput('00')}}>00</button>
+                <button onClick={() =>
+                    {handleInput('.')}}>.</button>
+                <button onClick={() =>
+                    {handleInput('=')}}>=</button>
+            </div>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
